b0d30f7f88a9094acb475391a826a8bb2fb7d38e
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-07-24 12:15:02 +0200

    done with navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>categories</h1>
+      <h1>posts</h1>
+      <h1>loadMore</h1>
+    </section>
   )
 }
 export default Home
---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import Image from "next/image"
+import { NavLinks } from "@/constants"
+import AuthProviders from "./AuthProviders"
+const Navbar = () => {
+  const session ={}
+  return (
+    <nav className="flexBetween navbar">
+        <div className="flex1 flexStart gap-10">
+            <Link href="/">
+                <Image src="/logo.svg" width={155} height={43} alt="flexibble"/>
+            </Link>
+            <ul className="xl:flex hidden text-small gap-7">
+                {NavLinks.map((link)=>(
+                  <Link href={link.href} key={link.key}>
+                    {link.text}
+                  </Link>
+                ))} 
+            </ul>
+        </div>
+
+        <div className="flexCenter gap-4">
+          {session ? (
+            <>userphoto
+            <Link href="/create-project">
+              Share work
+            </Link>
+            </>
+          ):(<AuthProviders />)}
+        </div>
+    </nav>
+  )
+}
+export default Navbar
